2035ebbd9d73f476e0452bdc74db5a74f94bbeed
DiegoSalazarn <ahrdaigo@gmail.com>
2024-12-19 18:02:47 -0300

    Añadi la page 404 Not Found.

--- 2da-entrega/src/App.jsx
+++ 2da-entrega/src/App.jsx
@@ -3,9 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 
-import Navbar from './components/navbar/navbar'; 
+import Navbar from './components/navbar/Navbar'; 
 import Catalog from './pages/Catalog'; 
 import ProductDetail from './pages/ProductDetail';
+import NotFound from './pages/NotFound';
 import ItemListContainer from './components/navbar/ItemListContainer/ItemListContainer';  
 
 import './App.css';
@@ -13,20 +14,29 @@ import './App.css';
 function App() {
   return (
     <Router>
-      {/* Barra de navegación */}
       <Navbar />
 
       <div className="app-content">
         <Routes>
-          {/* Página de inicio con saludo */}
+          {/* Página de inicio */}
           <Route 
-            path="/" element={<>{/* Saludo visible */}<ItemListContainer greeting="¡Bienvenido a nuestra tienda!" />{/* para agrega más contenido*/}</>} />
+            path="/" 
+            element={
+              <div style={{ textAlign: "center", marginTop: "20px" }}>
+                <h1>¡Bienvenido a nuestra tienda!</h1>
+                <ItemListContainer greeting="Explora nuestras categorías y encuentra lo que necesitas." />
+              </div>
+            } 
+          />
 
-            {/* Página de categorías */}
+          {/* Página de categorías */}
           <Route path="/category/:categoryId" element={<Catalog />} />
 
-            {/* Página de detalle de producto */}
+          {/* Página de detalle de producto */}
           <Route path="/product/:productId" element={<ProductDetail />} />
+
+          {/* Ruta para la página 404 */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </Router>
@@ -34,3 +44,4 @@ function App() {
 }
 
 export default App;
+
