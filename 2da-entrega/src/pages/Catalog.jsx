6788454ad7ccd4bf0e96ea8e7ebb19c3001f1bb1
DiegoSalazarn <ahrdaigo@gmail.com>
2024-12-19 16:46:38 -0300

    Cree archivos Css para cada archivo para mejorar la movilidad de las cosas y oprimizacion, tambien añadí que se puedan ver las img en details - entre otras cosas..

--- 2da-entrega/src/pages/Catalog.jsx
+++ 2da-entrega/src/pages/Catalog.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import { useParams, Link } from 'react-router-dom';
-import products from '../products'; // Importa los productos
-import ProductCard from './ProductCard'; // Suponiendo que tienes un componente ProductCard para mostrar los productos
+import products from '../products'; 
+import ProductCard from './ProductCard'; 
 
 function Catalog() {
-  const { categoryId } = useParams(); // Obtenemos el parámetro de la URL
+  const { categoryId } = useParams(); 
   
-  // Filtramos los productos según la categoría
   const filteredProducts = products.filter(product => product.category === categoryId);
 
   return (
     <div>
       <h2>Catálogo de {categoryId}</h2>
       <div className="product-list">
-        {/* Iteramos sobre los productos filtrados y mostramos el componente ProductCard */}
         {filteredProducts.length > 0 ? (
           filteredProducts.map(product => (
             <div key={product.id}>
@@ -21,7 +19,7 @@ function Catalog() {
             </div>
           ))
         ) : (
-          <p>No hay productos en esta categoría.</p>
+          <p>No existe ésta categoría</p>
         )}
       </div>
     </div>
